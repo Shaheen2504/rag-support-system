d9654574f3d349caee80240178dfa47e0bfd070d
Shaheen2504 <shaheenraqueeb@gmail.com>
2025-11-20 19:19:30 +0530

    Return metadata from retriever node

--- src/graph/retriever_node.py
+++ src/graph/retriever_node.py
@@ -14,3 +14,16 @@ def retrieve(state: AgentState, faiss_retriever) -> Dict[str, Any]:
         Dict[str, Any]: The updated graph state.
     """
 
+    # Retrieve the question
+    question = state["question"]
+
+    # Retrieve documents
+    documents = faiss_retriever.invoke(question)
+
+    # extracted metadata from the documents
+    metadata = [doc.metadata for doc in documents]
+
+    # Update the graph state
+    # state["documents"] = metadata
+
+    return {"documents": metadata}
